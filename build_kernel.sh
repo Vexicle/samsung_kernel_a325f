bc051feafbc357dc25793bf9e643d88f80369a2f
vexicle <wavetsuname@outlook.com>
2024-01-23 19:45:49 +0000

    Initial commit from WSL engine from original source, ahnet-69's android_kernel_samsung_a32.

---
+++ build_kernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CROSS_COMPILE=$(pwd)/../aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+export CC=$(pwd)/../clang-r383902/bin/clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export ARCH=arm64
+export ANDROID_MAJOR_VERSION=r
+
+export KCFLAGS=-w
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all) a32_defconfig -d
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all)
+
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+echo "is in arch/arm64/boot/Image"
