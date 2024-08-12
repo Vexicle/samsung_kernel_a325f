3a0c327ec1a332c3d24f5cb3ca3dfdd7074f3cb8
Vexicle <127143249+Vexicle@users.noreply.github.com>
2024-08-12 23:40:04 +0100

    optimize? build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,16 +6,11 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
 
-BLUE='\033[0;34m'
-NOCOLOUR='\033[0m'
-
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS="-w -O3 -fprofile-generate" CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all) a32_defconfig -d
-echo -e "${BLUE}==== PGO Complete ====${NOCOLOUR}"
-make -C $(pwd) O=$(pwd)/out KCFLAGS="-w -O3 -fprofile-use -march=native -mtune=native" CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all) a32_defconfig -d
-make -C $(pwd) O=$(pwd)/out KCFLAGS="-w -O3" CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all)
+make -C $(pwd) O=$(pwd)/out KCFLAGS="-w -O3" CONFIG_SECTION_MISMATCH_WARN_ONLY=y VERBOSE=1 -j$(nproc --all) a32_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS="-w -O3" CONFIG_SECTION_MISMATCH_WARN_ONLY=y VERBOSE=1 -j$(nproc --all)
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
-echo "Is in arch/arm64/boot/zImage"
+echo "If success, look in arch/arm64/boot for zImage file."
